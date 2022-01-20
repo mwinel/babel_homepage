06913761a87f19c84465395021c07c432a2649c1
Nelson M <mwinel@live.com>
2022-01-20 11:27:16 +0300

    Add terms of service page

---
+++ pages/terms-of-service.js
@@ -0,0 +1,100 @@
+import { useRouter } from 'next/router';
+import Layout from '../components/Layouts/Layout';
+import ContentPanel from '../components/ContentPanel/ContentPanel';
+
+export default function NotFound() {
+    return (
+        <Layout>
+            <ContentPanel>
+                <h1 className="my-10 font-extrabold tracking-tight text-center text-display-md">
+                    Terms of Service
+                </h1>
+                <p className="mb-6 text-md text-neutral-600">
+                    Last updated: December 31, 2021
+                </p>
+                <p className="mb-6 font-bold text-label-md text-neutral-800">
+                    Paragraph Title
+                </p>
+                <p className="mb-6 text-md text-neutral-800">
+                    Est aliquet odio at ultricies. Senectus nunc sit sed eget
+                    molestie etiam ultrices. Etiam neque libero, convallis
+                    consectetur id diam nullam. Nisi ullamcorper sapien, ipsum
+                    id amet volutpat tortor, sed semper. Libero sed aliquet
+                    facilisis sed. Venenatis elementum tempor, pellentesque
+                    tempus, vulputate libero. Bibendum amet placerat sit
+                    elementum vel. Sed enim eros, id quis ornare mi. Viverra
+                    iaculis orci, sed sollicitudin etiam eget elementum posuere
+                    nulla.
+                    <br />
+                    <br />
+                    Example bullet points:
+                    <br />
+                    <ul className="">
+                        <li>Point number 1</li>
+                        <li>Point number 2</li>
+                        <li>Point number 3</li>
+                    </ul>
+                    <br />
+                    <span className="font-bold">Inline Subtitle.</span> Bibendum
+                    eu, eget urna dolor in ullamcorper dolor. Integer id aliquam
+                    nisl in consectetur. Mi suspendisse nunc non, orci duis
+                    consequat diam. At massa tincidunt sed vel aliquam et
+                    volutpat. Duis est mauris sagittis aliquam enim diam.
+                    Bibendum enim, tristique tincidunt sed. Facilisis nisl
+                    posuere scelerisque vivamus amet. Tristique adipiscing
+                    lectus pellentesque phasellus sit quis dictum. Dui maecenas
+                    vulputate eu lorem blandit. Aliquam augue porttitor magna
+                    varius viverra mattis. Consectetur urna suscipit donec
+                    hendrerit elementum sit at diam. Eu rhoncus ullamcorper nisi
+                    tellus porttitor fermentum elementum nisl nunc. For more
+                    information:{' '}
+                    <a
+                        href="http://thisisalinktoawebsite.com/follow-this-format"
+                        className="text-primary-500 hover:text-primary-600 hover:underline"
+                    >
+                        http://thisisalinktoawebsite.com/follow-this-format
+                    </a>
+                    .
+                </p>
+                <p className="mb-6 font-bold text-label-md text-neutral-800">
+                    Paragraph Title
+                </p>
+                <p className="mb-6 text-md text-neutral-800">
+                    <span className="font-bold">Inline Subtitle.</span>
+                    <br />
+                    Est aliquet odio at ultricies. Senectus nunc sit sed eget
+                    molestie etiam ultrices. Etiam neque libero, convallis
+                    consectetur id diam nullam. Nisi ullamcorper sapien, ipsum
+                    id amet volutpat tortor, sed semper. Libero sed aliquet
+                    facilisis sed. Venenatis elementum tempor, pellentesque
+                    tempus, vulputate libero. Bibendum amet placerat sit
+                    elementum vel. Sed enim eros, id quis ornare mi. Viverra
+                    iaculis orci, sed sollicitudin etiam eget elementum posuere
+                    nulla.
+                    <br />
+                    <br />
+                    Risus libero non a, nulla velit, egestas nulla. Bibendum eu,
+                    eget urna dolor in ullamcorper dolor. Integer id aliquam
+                    nisl in consectetur. Mi suspendisse nunc non, orci duis
+                    consequat diam. At massa tincidunt sed vel aliquam et
+                    volutpat. Duis est mauris sagittis aliquam enim diam.
+                    Bibendum enim, tristique tincidunt sed. Facilisis nisl
+                    posuere scelerisque vivamus amet. Tristique adipiscing
+                    lectus pellentesque phasellus sit quis dictum. Dui maecenas
+                    vulputate eu lorem blandit. Aliquam augue porttitor magna
+                    varius viverra mattis. Consectetur urna suscipit donec
+                    hendrerit elementum sit at diam. Eu rhoncus ullamcorper nisi
+                    tellus porttitor fermentum elementum nisl nunc. For more
+                    information:{' '}
+                    <a
+                        href="http://thisisalinktoawebsite.com/follow-this-format"
+                        className="text-primary-500 hover:text-primary-600 hover:underline"
+                    >
+                        http://thisisalinktoawebsite.com/follow-this-format
+                    </a>
+                    .
+                </p>
+            </ContentPanel>
+        </Layout>
+    );
+}
